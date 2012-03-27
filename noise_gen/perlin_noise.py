52552b3fca58416a5ad0cc2da78a6f48f2c91a23
Chirag Khatri <ckhatri4@gmail.com>
2012-03-27 19:44:13 -0400

    Added 2d perlin noise and cleaned up code. Removed plotting functions for now.

--- noise_gen/perlin_noise.py
+++ noise_gen/perlin_noise.py
@@ -1,7 +1,5 @@
 import math
 from random import random
-import matplotlib.pyplot as plt
-
 # ------------------------------------------------------------------------
 # If you want to generate plots and experiment with the noise funciton,
 # you must install matplotlib. Instructions on how to do so can be found
@@ -9,14 +7,9 @@ import matplotlib.pyplot as plt
 # also uncomment the code in the perlin noise function.
 # ------------------------------------------------------------------------
 
-# Simply returns n random numbers between [0, 1]
-def generate_white_noise(n):
-	noise = []
-	for x in range(0, n):
-		noise.append(random())
-	return noise
-
+# -----------------------------------------
 # Interpolation methods
+# -----------------------------------------
 def linear_interpolation(a, b, x):
 	return a*(1-x) + b*x
 
@@ -31,43 +24,121 @@ def sample_points(x, t, max_x):
 	x0 = x // t * t # '//' simply floors result of 'x / t'
 	return x0, (x0 + t) % max_x, (x - x0) / t
 
-# Interpolates between all of the base_noise values using interpolation_fun
-def generate_smooth_noise(base_noise, interpolation_fun, octave):
+# -----------------------------------------
+# White Noise Generation
+# -----------------------------------------
+
+# Fills a list of size n with random values between [0, 1]
+def generate_white_noise_1d(n):
+	noise = []
+	for x in range(0, n):
+		noise.append(random())
+	return noise
+
+# Fills a 2d array [width x height] with random values between [0, 1]
+def generate_white_noise_2d(width, height):
+	noise = []
+	for i in range(width):
+		noise.append([])
+		for j in range(height):
+			noise[i].append(random())
+	return noise
+
+# -----------------------------------------
+# Smooth Noise Geneartion
+# -----------------------------------------
+
+# Interpolates between all of the base_noise values using the specefied
+# interpolation function. Defaults to linear interpolation if none specefied
+def generate_smooth_noise_1d(base_noise, interpolation, octave):
+	if(interpolation == 'linear'):
+		interpolation_fun = linear_interpolation
+	else:
+		interpolation_fun = cosine_interpolation
+
 	noise_len = len(base_noise)
 	t = 2**octave
 	smooth_noise = []
-	
 	for i in range(noise_len):	
 		a, b, x = sample_points(i, t, noise_len)							
 		smooth_noise.append(interpolation_fun(base_noise[a], base_noise[b], x))
 	return smooth_noise
-	
 
-def perlin_noise_1d(layers, falloff, base_noise):
+def generate_smooth_noise_2d(base_noise, octave):
+
+	width = len(base_noise)
+	height = len(base_noise[0])
+	t = 2**octave
+	smooth_noise = []
+
+	for i in range(width):	
+		smooth_noise.append([])
+		x0, x1, xalpha = sample_points(i, t, width)							
+		for j in range(height):
+			y0, y1, yalpha = sample_points(j, t, height)
+			a0 = (1 - xalpha)*base_noise[x0][y0] +(xalpha)*base_noise[x1][y0]
+			a1 = (1 - xalpha)*base_noise[x0][y1] + (xalpha)*base_noise[x1][y1]
+			smooth_noise[i].append(linear_interpolation(a0, a1, yalpha))
+	return smooth_noise
+
+# -----------------------------------------
+# Perlin Noise Generation
+# -----------------------------------------
+# Basic idea of algorithm:
+# 	1) Generate random noise values
+# 	2) 'Smoothen' each random value out so it appears less random.
+# 	   This is done for each octave iteration.
+# 	3) Combine all of the smooth values 
+# 	4) Normalize the values so that they are between [0, 1]
+
+def perlin_noise_1d(n, layers, persistence, interpolation='linear'):
+	base_noise = generate_white_noise_1d(n)
 	perlin_noise = []
-	for x in range(len(base_noise)):
+	for x in range(n):
 		perlin_noise.append(0)
 
-	r = 1
+	amplitude = 1
+	total_amplitude = 0
 	for k in range(layers):
-		r *= falloff
-		smooth_noise = generate_smooth_noise(base_noise, linear_interpolation, layers - k - 1)
-		# plt.axis([0, 100, 0, 1])
-		# plt.plot(range(100), smooth_noise)
-		# plt.savefig("octave_%s" % k)
-		# plt.clf()
+		amplitude *= persistence
+		total_amplitude += amplitude
+		smooth_noise = generate_smooth_noise_1d(base_noise, interpolation, layers - k - 1)
 
-		for p in range(len(perlin_noise)):
-			perlin_noise[p] += smooth_noise[p]*r
+		for p in range(n):
+			perlin_noise[p] += smooth_noise[p] * amplitude
 
-	# plt.axis([0, 100, 0, 1])
-	# plt.plot(range(100),perlin_noise)
-	# plt.savefig("perlin")
+	for p in range(n):
+		perlin_noise[p] /= total_amplitude
+			
 	return perlin_noise
 			
-# Debugging...
-sample_noise = generate_white_noise(100)
-perlin_noise = perlin_noise_1d(6, .5, sample_noise)
+def perlin_noise_2d(width, height, layers, persistence):
+	base_noise = generate_white_noise_2d(width, height)
+	perlin_noise = []
+	for i in range(width):
+		perlin_noise.append([])
+		for j in range(height):
+			perlin_noise[i].append(0)
 
-for x in perlin_noise:
-	print x
+	amplitude = 1
+	total_amplitude = 0
+	for k in range(layers):
+		amplitude *= persistence
+		total_amplitude += amplitude
+		smooth_noise = generate_smooth_noise_2d(base_noise, layers - k - 1)
+
+		for i in range(width):
+			for j in range(height):
+				perlin_noise[i][j] += smooth_noise[i][j] * amplitude
+
+	for i in range(width):
+		for j in range(height):
+			perlin_noise[i][j] /= total_amplitude
+
+	return perlin_noise
+
+# Debugging...
+perlin_noise = perlin_noise_2d(20, 5, 6, .5)
+
+for p in perlin_noise:
+	print p
