a460027d4410aa3bd0eda64cfb2ac9a03aa51f62
Matt Garnes <mattgmg1990@hotmail.com>
2012-04-02 02:09:07 -0400

    Started an XNA project that does nothing but draw an XWing on the screen. Still working on the camera class, it isn't working right now. It has some classes that can serve as the framework for our demo. We can work with this project to create the demo.

---
+++ CS4300-Final-Project/CS4300-Final-Project/CS4300-Final-Project/Camera.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CS4300_Final_Project
+{
+    class Camera
+    {
+        Vector3 cameraPosition = new Vector3(130, 30, -50);
+        float leftrightRot = MathHelper.PiOver2;
+        float updownRot = -MathHelper.Pi / 10.0f;
+        const float rotationSpeed = 0.3f;
+        const float moveSpeed = 30.0f;
+        Matrix viewMatrix;
+        GraphicsDevice mDevice;
+
+        MouseState originalMouseState;
+
+        public Camera(GraphicsDevice device, Matrix view)
+        {
+            mDevice = device;
+            Mouse.SetPosition(device.Viewport.Width / 2, device.Viewport.Height / 2);
+            originalMouseState = Mouse.GetState();
+            viewMatrix = view;
+        }
+
+
+
+        private Matrix UpdateViewMatrix()
+        {
+            Matrix cameraRotation = Matrix.CreateRotationX(updownRot) * Matrix.CreateRotationY(leftrightRot);
+
+            Vector3 cameraOriginalTarget = new Vector3(0, 0, -1);
+            Vector3 cameraRotatedTarget = Vector3.Transform(cameraOriginalTarget, cameraRotation);
+            Vector3 cameraFinalTarget = cameraPosition + cameraRotatedTarget;
+
+            Vector3 cameraOriginalUpVector = new Vector3(0, 1, 0);
+            Vector3 cameraRotatedUpVector = Vector3.Transform(cameraOriginalUpVector, cameraRotation);
+
+            return Matrix.CreateLookAt(cameraPosition, cameraFinalTarget, cameraRotatedUpVector);
+        }
+
+        public Matrix ProcessInput(float amount)
+        {
+            MouseState currentMouseState = Mouse.GetState();
+            if (currentMouseState != originalMouseState)
+            {
+                float xDifference = currentMouseState.X - originalMouseState.X;
+                float yDifference = currentMouseState.Y - originalMouseState.Y;
+                leftrightRot -= rotationSpeed * xDifference * amount;
+                updownRot -= rotationSpeed * yDifference * amount;
+                Mouse.SetPosition(mDevice.Viewport.Width / 2, mDevice.Viewport.Height / 2);
+                UpdateViewMatrix();
+            }
+            Vector3 moveVector = new Vector3(0, 0, 0);
+            KeyboardState keyState = Keyboard.GetState();
+            if (keyState.IsKeyDown(Keys.Up) || keyState.IsKeyDown(Keys.W))
+                moveVector += new Vector3(0, 0, -1);
+            if (keyState.IsKeyDown(Keys.Down) || keyState.IsKeyDown(Keys.S))
+                moveVector += new Vector3(0, 0, 1);
+            if (keyState.IsKeyDown(Keys.Right) || keyState.IsKeyDown(Keys.D))
+                moveVector += new Vector3(1, 0, 0);
+            if (keyState.IsKeyDown(Keys.Left) || keyState.IsKeyDown(Keys.A))
+                moveVector += new Vector3(-1, 0, 0);
+            if (keyState.IsKeyDown(Keys.Q))
+                moveVector += new Vector3(0, 1, 0);
+            if (keyState.IsKeyDown(Keys.Z))
+                moveVector += new Vector3(0, -1, 0);
+            return AddToCameraPosition(moveVector * amount);
+        }
+
+        private Matrix AddToCameraPosition(Vector3 vectorToAdd)
+        {
+            Matrix cameraRotation = Matrix.CreateRotationX(updownRot) * Matrix.CreateRotationY(leftrightRot);
+            Vector3 rotatedVector = Vector3.Transform(vectorToAdd, cameraRotation);
+            cameraPosition += moveSpeed * rotatedVector;
+            return UpdateViewMatrix();
+        }
+
+    }
+}
---
+++ CS4300-Final-Project/CS4300-Final-Project/CS4300-Final-Project/ModelObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+
+namespace CS4300_Final_Project
+{
+    abstract class ModelObject
+    {
+        public Model mModel;
+        public abstract void load(ContentManager content, Effect effect);
+        public abstract void draw(Matrix viewMatrix, Matrix projectionMatrix);
+    }
+}
