a07426f4e171309fc59521fd74d064abd3b185fc
Kenny Wong <kwong.ccs@gmail.com>
2012-04-16 12:52:18 -0400

    For #12: Made a sky (sky + clouds) texture with a "sky blue" color.
    Tried implementing skydome but non-functional yet, so not committing
    that part yet. (#9) Hopefully this should be a usable sky texture
    though.

--- noise_gen/perlin_generator.py
+++ noise_gen/perlin_generator.py
@@ -9,16 +9,16 @@ def generate_sample_textures():
 	octaves = 9
 
 	# generate cloud texture
-##	gradient = Gradient((1, 1, 1, 1), (0, 0, 1, 1))
-##	p_noise = perlin_noise_2d(w, h, octaves, .5)
-##	color_grid = map_gradient(gradient, p_noise)
-##	generate_texture(color_grid, 'cloud_texture.png')
-
-        # generate black and white texture
-	gradient = Gradient((0, 0, 0, 1), (1, 1, 1, 1))
-	p_noise = perlin_noise_2d(w, h, octaves, .3)
+	gradient = Gradient((1, 1, 1, 1), ((135.0/255.0), (206.0/255.0), (250.0/255.0), 1))
+	p_noise = perlin_noise_2d(w, h, octaves, .5)
 	color_grid = map_gradient(gradient, p_noise)
-	generate_texture(color_grid, 'perlin_noise.png')
+	generate_texture(color_grid, 'cloud_texture.png')
+
+##        # generate black and white texture
+##	gradient = Gradient((0, 0, 0, 1), (1, 1, 1, 1))
+##	p_noise = perlin_noise_2d(w, h, octaves, .3)
+##	color_grid = map_gradient(gradient, p_noise)
+##	generate_texture(color_grid, 'perlin_noise.png')
 
 ##        # generate grass and dirt ground texture
 ##	gradient = Gradient((.62, .32, .17, 1), ((34.0/255.0), (139.0/255.0), (34.0/255.0), 1))
